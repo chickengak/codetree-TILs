919afb35f9793f14ee40a89b3e953a52f34fd909
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2024-02-03 22:44:24 +0900

    Title: 공약수의 유무, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240203/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,4 +1,12 @@
-from math import gcd
+cd = [1]
+for n in range(2, int(1920**0.5)+1):
+    if 1920%n==0 and 2880%n==0:
+        cd += [n, 1920//n]
 
 a, b = list(map(int, input().split()))
-print(1 if gcd(a, b)>1 else 0)
+for n in range(a, b+1):
+    if n in cd:
+        print(1)
+        break
+else:
+    print(0)
