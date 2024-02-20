f8b78fd1777a638f836f279212e58e45b3d496f4
Jinhyeong Jang <37939855+chickengak@users.noreply.github.com>
2024-02-21 03:25:05 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
+++ 240221/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
@@ -1,6 +1,6 @@
 from math import ceil, floor
 
 nums = list(map(float, input().split()))
-nums[nums.index(temp)] = ceil((temp:=max(nums)))
-nums[nums.index(temp)] = floor((temp:=min(nums)))
-print(*[n if isinstance(n, int) else round(n) for n in nums ])
+print(ceil(nums.pop(nums.index(max(nums)))), end = ' ')
+print(floor(nums.pop(nums.index(min(nums)))), end = ' ')
+print(round(nums.pop()))
